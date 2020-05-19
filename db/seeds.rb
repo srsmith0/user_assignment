dc8608a1b6318219c94e35ebd882fac3aa9c029a
Shawn Smith <srsmith11@live.com>
2020-05-19 17:10:38 -0600

    added faker seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+# t.string "first_name"
+# t.integer "age"
+# t.string "phone"
+# t.string "email"
+# t.string "last_name"
+# t.boolean "friend"
+# t.string "first_name"
+#     t.string "last_name"
+#     t.integer "age"
+#     t.string "hair_color"
+#     t.string "eye_color"
+#     t.string "gender"
+#     t.boolean "alive"
+10.times do 
+  user = User.create(
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.last_name,
+    age: rand(18...65),
+    hair_color: Faker::Color.color_name,
+    eye_color: Faker::Color.color_name,
+    gender: "M" || "F",
+    alive: true || false
+    )
+
+  end
+
