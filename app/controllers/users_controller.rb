d3c833ffe243cea95ff56a9ac0714ade68cc44d4
Shawn Smith <srsmith11@live.com>
2020-05-19 16:49:27 -0600

    added edit, delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,36 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if(@user.update(users_params))
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(users_params) 
       redirect_to users_params
     else
       render :edit
+    end
   end
-end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :age, :hair_color, :eye_color, :gender, :alive)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # get 'users/new'
   # get 'users/edit'
   root 'users#index'
-  resource :users
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
