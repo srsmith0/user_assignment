f4898187deddfd33b411b14f5d029cfc188f1573
Shawn Smith <srsmith11@live.com>
2020-05-19 16:10:02 -0600

    added controller, view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
+    if(@user.update(users_params))
+      redirect_to users_params
+    else
+      render :edit
   end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  root 'users#index'
+  resource :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
